06202ee10bcede2d87973a690169346a05867188
Ochi <ochi@arcor.de>
2013-10-10 23:42:51 +0200

    Added Ptr and WPtr typedefs

--- Library/Rendered/Cube.h
+++ Library/Rendered/Cube.h
@@ -11,6 +11,10 @@ namespace Rendered {
 
 class Cube {
 	public:
+		typedef std::shared_ptr<Cube> Ptr;
+		typedef std::weak_ptr<Cube>   WPtr;
+	
+	public:
 		Cube(const Eigen::Affine3f& trafo, const Eigen::Vector4f& ambientColor = Eigen::Vector4f(0.4f, 0.4f, 0.5f, 1.f), const Eigen::Vector4f& diffuseColor = Eigen::Vector4f(1.f, 1.f, 1.f, 1.f));
 
 		void render(const Eigen::Matrix4f& mvMatrix, const Eigen::Matrix4f& prMatrix, const Eigen::Matrix3f& nmMatrix);
