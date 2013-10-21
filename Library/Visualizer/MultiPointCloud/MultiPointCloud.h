4141149c4ffe487e63fe081ba73fb71fe2252c3f
Paul Hilbert <me@sicgraph.org>
2013-10-21 20:45:53 +0200

    merged MultiPointCloud base vis and base skeleton fix from effects

---
+++ Library/Visualizer/MultiPointCloud/MultiPointCloud.h
@@ -0,0 +1,60 @@
+#ifndef MULTIPOINTCLOUDVIS_H_
+#define MULTIPOINTCLOUDVIS_H_
+
+#include <FW/Visualizer.h>
+
+#include <Geometry/PCLTools.h>
+using Geometry::PCLTools;
+typedef pcl::PointNormal Point;
+typedef PCLTools<Point>  Tools;
+typedef Tools::CloudType Cloud;
+typedef Tools::Idx       Idx;
+typedef Tools::IdxSet    IdxSet;
+
+#include <Library/Rendered/Cloud.h>
+#include <Library/Rendered/Vectors.h>
+#include <Library/Rendered/Lines.h>
+
+namespace FW {
+
+
+class MultiPointCloud : virtual public Visualizer {
+	public:
+		typedef std::shared_ptr<MultiPointCloud> Ptr;
+		typedef std::weak_ptr<MultiPointCloud>   WPtr;
+		typedef Rendered::Field                  RF;
+		typedef Rendered::Cloud                  RC;
+		typedef Rendered::Vectors                RV;
+		typedef Rendered::Lines                  RL;
+		typedef std::map<std::string, RF::Ptr>   RFPtrs;
+		friend class Computation;
+
+	public:
+		MultiPointCloud(std::string id, const GUI::Property::Paths& paths);
+		virtual ~MultiPointCloud();
+
+		void init();
+		void render();
+		void addProperties();
+		void registerEvents();
+
+	protected:
+		void    addClouds(const GUI::Property::Paths& paths);
+		RC::Ptr addCloud(std::string name, RGBA color, const std::vector<Vector3f>& points, bool visible = true);
+		RC::Ptr addCloud(std::string name, RGBA color, Cloud::Ptr cloud, bool visible = true);
+		RV::Ptr addNormals(std::string name, RGBA color, Cloud::Ptr cloud, bool visible = true);
+		RL::Ptr addLines(std::string name, RGBA color, const std::vector<Vector3f>& points, bool visible = true);
+		void    exportCloud(const fs::path& path);
+		void    resample();
+	
+	protected:
+		GUI::Property::Paths     m_paths;
+		Cloud::Ptr               m_cloud;
+		RFPtrs                   m_rf;
+};
+
+#include "MultiPointCloud.inl"
+
+} // FW
+
+#endif /* MULTIPOINTCLOUDVIS_H_ */
